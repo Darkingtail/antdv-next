01a6fc45ffd79f1b3d4ccf4819df35a86f6e37a0
aibayanyu20 <aibayanyu@qq.com>
2025-11-24 15:28:25 +0800

    chore: change

--- playground/src/pages/switch/demo/text.vue
+++ playground/src/pages/switch/demo/text.vue
@@ -3,12 +3,14 @@ import { CheckOutlined, CloseOutlined } from '@antdv-next/icons'
 import { shallowRef } from 'vue'
 
 const checked = shallowRef()
+const checked1 = shallowRef()
+const checked2 = shallowRef()
 </script>
 
 <template>
   <a-switch v-model:checked="checked" checked-children="开启" un-checked-children="关闭" default-checked />
-  <a-switch v-model:checked="checked" checked-children="1" un-checked-children="0" default-checked />
-  <a-switch v-model:checked="checked" default-checked>
+  <a-switch v-model:checked="checked1" checked-children="1" un-checked-children="0" />
+  <a-switch v-model:checked="checked2" default-checked>
     <template #checkedChildren>
       <CheckOutlined />
     </template>
